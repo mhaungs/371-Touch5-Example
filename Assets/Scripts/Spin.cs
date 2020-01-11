4c9cc36b0d078ac1a72bceca397212cdb827155d
mhaungs <mhaungs@calpoly.edu>
2020-01-11 11:01:04 -0800

    Stage 2:  Added Spin script to all Targets

---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    public float speed = 2f;
+    private Vector3 ySpin = new Vector3(0, 1, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ySpin *= speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(ySpin);
+    }
+}
