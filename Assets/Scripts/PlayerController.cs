f4fec7d89588fd84827e657f8c59cb5e69bd300e
mhaungs <mhaungs@calpoly.edu>
2020-01-11 11:04:49 -0800

    Stage 3:  Added player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 translation = new Vector3(horizontalInput, 0, verticalInput);
+
+        transform.Translate(translation * Time.deltaTime * speed);
+    }
+}
