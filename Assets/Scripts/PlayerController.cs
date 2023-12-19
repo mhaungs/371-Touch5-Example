f203834bd116700f97bfd207b3003adf3b8d81e8
Michael Haungs <mhaungs@calpoly.edu>
2023-12-19 14:50:02 -0800

    Updated to New Input System
    Installed Input System package and updated player controls to use the new system.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,28 +1,34 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float speed = 10f;
+
+    // Input
+    PlayerInput _playerInput;
+    InputAction _moveAction;
 
     Vector3 _translation;
     Rigidbody rgbd;
 
     void Awake()
     {
+        PlayerInput _playerInput = GetComponent<PlayerInput>();
+        _moveAction = _playerInput.actions["Movement"];
+
         rgbd = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        _translation = new Vector3(horizontalInput, 0, verticalInput);
-        _translation = _translation.normalized;  // so moving diagonally is not faster
+        Vector2 moveInput = _moveAction.ReadValue<Vector2>();
+        _translation = new Vector3(moveInput.x, 0, moveInput.y);
     }
 
     void FixedUpdate()
     {
-        rgbd.MovePosition(transform.position + _translation  * Time.deltaTime * speed);
+        rgbd.MovePosition(transform.position + _translation * Time.deltaTime * speed);
     }
 
     void OnTriggerEnter(Collider other)
